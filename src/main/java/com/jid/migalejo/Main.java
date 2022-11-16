ecc443978eb49be3c9d2f15740253e4185c8261c
Miguel Correa <migalejo85@gmail.com>
2022-11-16 19:33:46 +0100

    Section 1-3 completed

---
+++ src/main/java/com/jid/migalejo/Main.java
@@ -0,0 +1,15 @@
+package com.jid.migalejo;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int test = 'Ñ';
+        System.out.println(test);
+
+    }
+
+
+
+}
---
+++ src/main/java/com/jid/migalejo/Seccion3/CodingExercise_1/Instructor.java
@@ -0,0 +1,68 @@
+package com.jid.migalejo.Seccion3.CodingExercise_1;
+
+
+public class Instructor {
+    public long id;
+    public String name;
+    public String title;
+    public String department;
+    public Book[] books;
+
+    public Instructor(long id, String name, String title, String department, Book[] books) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.department = department;
+        this.books = books;
+
+    }
+
+    public Instructor(long id, String name, String title, String department) {
+        this(id,name,title,department,null);
+    }
+
+    public Instructor(long id, String name, String title) {
+        this(id,name,title,null,null);
+    }
+
+    public Instructor(long id, String name) {
+        this(id,name,null,null,null);
+    }
+
+    public String getMostRecentBookTitle() {
+        if (books != null){
+            return books[books.length-1].title;
+        }else return "no books found :-(";
+    }
+
+    public Book updateBook(int index, String title) {
+        Book temp = null;
+        if (books != null) {
+            temp = books[index];
+            books[index] = new Book(title);
+        }
+        return temp;
+    }
+
+    public Book updateBook(int index, Book book) {
+        Book temp = null;
+        if (books != null){
+            temp = books[index];
+            books[index] = book;
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Java for Beginners");
+        Book book2 = new Book("Scala for Beginners");
+        Book book3 = new Book("Effective Python");
+
+        Instructor instructor = new Instructor(101, "John", "Assistant Professor", "Computer Science", new Book[]{book1, book2, book3});
+        System.out.println(instructor.getMostRecentBookTitle());
+        System.out.println("old book title: " + instructor.updateBook(1, "Effective C#").getTitle());
+
+        Book book4 = new Book("Introduction to Data Mining");
+        System.out.println("old book title: " + instructor.updateBook(1, book4).getTitle());
+    }
+}
