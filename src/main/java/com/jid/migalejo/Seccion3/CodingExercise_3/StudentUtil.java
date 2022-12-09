f2032b4bcd6d62c6b60035941e2dcc8bdd8394fb
Miguel Correa <migalejo85@gmail.com>
2022-12-09 16:56:00 +0100

    Coding Exercise 2 solved and 3 prepared to start solving

---
+++ src/main/java/com/jid/migalejo/Seccion3/CodingExercise_3/StudentUtil.java
@@ -0,0 +1,19 @@
+package com.jid.migalejo.Seccion3.CodingExercise_3;
+public class StudentUtil {
+
+    public static double[] calculateGPA(int[] studentIdList, char[][] studentsGrades) {
+        // your code
+
+        return null;
+    }
+
+    public static int[] getStudentsByGPA(double lower, double higher, int[] studentIdList, char[][] studentsGrades) {
+        // perform parameter validation. Return null if passed parameters are not valid
+
+        // invoke calculateGPA
+
+        // construct the result array and return it. You would need an extra for loop to compute the size of the resulting array
+    return null;
+    }
+
+}
